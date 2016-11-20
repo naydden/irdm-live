60ef38e21b43efbeb6bd8dd040207292d7306366
polmes <pol.developer1@gmail.com>
2016-11-20 11:07:03 +0100

    session test

--- stream.php
+++ stream.php
@@ -4,8 +4,18 @@ header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
 // We'll be using PHP_EOL instead of \n for multi-arch compatibility
-echo "id: " . time() . PHP_EOL;
-echo 'data: { "id": "' . time() . '", "latitude": "' . time() . '"}' . PHP_EOL;
-echo PHP_EOL;
-ob_flush();
-flush();
+// echo "id: " . time() . PHP_EOL;
+// echo 'data: { "id": "' . time() . '", "latitude": "' . time() . '"}' . PHP_EOL;
+// echo PHP_EOL;
+// ob_flush();
+// flush();
+
+session_start();
+if (isset($_SESSION['data'])) {
+	// We'll be using PHP_EOL instead of \n for multi-arch compatibility
+	echo "id: " . time() . PHP_EOL;
+	echo 'data: { "id": "' . time() . '", "latitude": "' . $_SESSION['data'] . '"}' . PHP_EOL;
+	echo PHP_EOL;
+	ob_flush();
+	flush();
+}
